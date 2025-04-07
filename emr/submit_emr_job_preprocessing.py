8d4c2218ede9c4e3e61c9663a50291bf7877d3ce
Lambert TEADJEU DONGFACK <62360058+lamgtax01@users.noreply.github.com>
2025-04-07 12:25:05 -0400

    Add files via upload

---
+++ emr/submit_emr_job_preprocessing.py
@@ -0,0 +1,55 @@
+import boto3
+import json
+import os
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--region", default="us-east-1")
+args = parser.parse_args()
+
+s3 = boto3.client("s3", region_name=args.region)
+emr = boto3.client("emr-serverless", region_name=args.region)
+
+bucket = os.environ["S3_BUCKET"]
+prefix = os.environ["S3_PREFIX"]
+job_type = os.environ["JOB_TYPE"]
+app_info_key = os.environ["APP_INFO_KEY"]
+entry_point = os.environ["ENTRY_POINT"]
+emr_role = os.environ["EMR_ROLE"]
+
+# Load app_id
+obj = s3.get_object(Bucket=bucket, Key=app_info_key)
+app_id = json.loads(obj["Body"].read())["applicationId"]
+
+# Submit EMR Job
+response = emr.start_job_run(
+    applicationId=app_id,
+    executionRoleArn=emr_role,
+    jobDriver={
+        "sparkSubmit": {
+            "entryPoint": entry_point,
+            "entryPointArguments": [
+                "--input", f"s3://{bucket}/{prefix}/data/",
+                "--output", f"s3://{bucket}/{prefix}/processing/"
+            ]
+        }
+    },
+    configurationOverrides={
+        "monitoringConfiguration": {
+            "s3MonitoringConfiguration": {
+                "logUri": f"s3://{bucket}/{prefix}/logs/"
+                }
+            }
+        }
+)
+
+job_id = response["jobRunId"]
+print(f"✅ Submitted EMR job ({job_type}) with ID: {job_id}")
+print(f"✅ Log will be stored at s3://{bucket}/{prefix}/logs/{app_id}/{job_id}/")
+
+# Save job ID to S3
+s3.put_object(
+    Bucket=bucket,
+    Key=f"{prefix}/emr-tracking/{job_type}_job_info.json",
+    Body=json.dumps({"jobRunId": job_id})
+)
